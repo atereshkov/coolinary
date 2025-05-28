d02c59f682a73f367c7bab611202a73850e3741f
Aliaksandr Tserashkou <atserashkou@gmail.com>
2025-05-28 22:44:53 +0200

    Initial

---
+++ CoolinaryTests/CoolinaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoolinaryTests.swift
+//  CoolinaryTests
+//
+//  Created by Aliaksandr Tserashkou on 25/05/2025.
+//
+
+import Testing
+@testable import Coolinary
+
+struct CoolinaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ coolinary/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Coolinary
+//
+//  Created by Aliaksandr Tserashkou on 25/05/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
