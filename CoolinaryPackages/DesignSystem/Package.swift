d6817d1a9618aa66e695bcbe97effbe1a06f9f33
Aliaksandr Tserashkou <atserashkou@gmail.com>
2025-06-20 23:39:49 +0200

    Dependency package

--- CoolinaryPackages/DesignSystem/Package.swift
+++ CoolinaryPackages/DesignSystem/Package.swift
@@ -4,28 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "HomeFlow",
+    name: "DesignSystem",
     platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "HomeFlow",
-            targets: ["HomeFlow"]),
-    ],
-    dependencies: [
-        .package(path: "../../CoolinaryPackages/DesignSystem")
+            name: "DesignSystem",
+            targets: ["DesignSystem"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HomeFlow",
-            dependencies: [
-                "DesignSystem"
-            ]),
+            name: "DesignSystem"),
         .testTarget(
-            name: "HomeFlowTests",
-            dependencies: ["HomeFlow"]
+            name: "DesignSystemTests",
+            dependencies: ["DesignSystem"]
         ),
     ]
 )
---
+++ CoolinaryPackages/DesignSystem/Sources/DesignSystem/DesignSystem.swift
@@ -0,0 +1,7 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct DesignSystemConfig {
+    public init() {}
+    public let test: String = "123"
+}
